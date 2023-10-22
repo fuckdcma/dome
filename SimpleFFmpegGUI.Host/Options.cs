0b96defd3912c284e8ad036007647c2adb598c63
sonaprv <140211636+sonaprv@users.noreply.github.com>
2023-10-22 21:26:53 +0700

    Update Options.cs

--- SimpleFFmpegGUI.Host/Options.cs
+++ SimpleFFmpegGUI.Host/Options.cs
@@ -4,16 +4,16 @@ namespace SimpleFFmpegGUI
 {
     internal class Options
     {
-        [Option('p', Required = false, HelpText = "命名管道名称")]
+        [Option('p', Required = false, HelpText = "Đặt tên cho pipe")]
         public string PipeName { get; set; }
 
-        [Option('s', Default = false, Required = false, HelpText = "注册开机启动")]
+        [Option('s', Default = false, Required = false, HelpText = "Đăng ký và bắt đầu")]
         public bool RegisterStartup { get; set; }
 
-        [Option('u', Default = false, Required = false, HelpText = "取消开机启动")]
+        [Option('u', Default = false, Required = false, HelpText = "Hủy khởi động")]
         public bool UnregistereStartup { get; set; }
 
-        [Option('d', Default = false, Required = false, HelpText = "设置工作目录为程序所在目录")]
+        [Option('d', Default = false, Required = false, HelpText = "Đặt thư mục làm việc vào thư mục chứa chương trình")]
         public bool WorkingDirectoryHere { get; set; }
     }
 }
