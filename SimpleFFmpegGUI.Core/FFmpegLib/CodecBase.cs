7fad5425df16536e986156be85d202613299dbc8
sonaprv <140211636+sonaprv@users.noreply.github.com>
2023-10-22 18:01:58 +0700

    dịch CodecBase.cs
    dịch CodecBase.cs

--- SimpleFFmpegGUI.Core/FFmpegLib/CodecBase.cs
+++ SimpleFFmpegGUI.Core/FFmpegLib/CodecBase.cs
@@ -3,12 +3,12 @@
     public abstract class CodecBase
     {
         /// <summary>
-        /// 编码名称
+        /// Tên codec
         /// </summary>
         public abstract string Name { get; }
 
         /// <summary>
-        /// 编码在FFmpeg中的库名称
+        /// Tên thư viện codec trong FFmpeg
         /// </summary>
         public abstract string Lib { get; }
     }
